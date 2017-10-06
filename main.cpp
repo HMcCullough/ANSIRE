7560930210f2c3c04ed8f08f6e94f56cb80402e1
WilliamsonLogan <williamson.logan@gmail.com>
2017-10-05 17:29:04 -0700

    Test main, not functional

--- main.cpp
+++ main.cpp
@@ -4,16 +4,16 @@
 int main(void) {
   ASCIIDisplay ad;
 
-  for (int y = 0; y < ScreenHeight; y++) {
+  /*for (int y = 0; y < ScreenHeight; y++) {
     for (int x = 0; x < ScreenWidth; x++) {
       ad.charSet('0' + x, x, y);
     }
-  }
+  }*/
+
+  ad.CreateDialogue(60, 15, 5, 1);
 
   ad.Print("|        TEST        |", 20, 11);
   ad.Print("|        TEST        |", 20, 13, true);
-
-  ad.CreateDialogue(20, 5, 1, 1);
 
   ad.draw();
 }
