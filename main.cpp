fb1195f1f7ff088e9d5156c8251d59038d907bb3
hmccullo <hunter.mccullough@wsu.edu>
2017-10-06 22:58:19 -0700

    Began work on the sdl_window class wrapper

--- main.cpp
+++ main.cpp
@@ -1,19 +1,26 @@
-#include "ASCIIDisplay.h"
+#include "sdl_window.h"
 
 // Sample main, prints lines of characters and then layers TEST in the middle.
 int main(void) {
-  ASCIIDisplay ad;
 
-  /*for (int y = 0; y < ScreenHeight; y++) {
-    for (int x = 0; x < ScreenWidth; x++) {
-      ad.charSet('0' + x, x, y);
-    }
-  }*/
+	sdl_window window(100, 100, "Title");
+	window.init();
 
-  ad.CreateDialogue(60, 15, 5, 1);
+	SDL_Event e;
+	bool quit = false;
 
-  ad.Print("|        TEST        |", 20, 11);
-  ad.Print("|        TEST        |", 20, 13, true);
+	while (!quit)
+		while (SDL_PollEvent(&e) != 0)
+		{
+			switch (e.type)
+			{
+				case SDL_QUIT:
+					quit = true;
+					break;
+				default: break;
+			}
+		}
+	window.close();
 
-  ad.draw();
+	return 0;
 }
---
+++ sdl_window.h
@@ -0,0 +1,85 @@
+#include <SDL2/SDL.h>
+#include <iostream>
+#include <string>
+
+using std::string;
+using std::cout;
+
+class sdl_window
+{
+public:
+	sdl_window(int _width, int _height, string _title)
+	{
+		window = nullptr;
+		surface = nullptr;
+
+		width = _width;
+		height = _height;
+		title = _title;
+	}
+
+	~sdl_window()
+	{
+		close();
+	}
+
+	/// Used for initializing the window/SDL sub-systems
+	/// This will make an SDL_Window struct with the parameters passed into the constructor
+	/// The function returns a boolean false if any of the SDL sub-systems did not initialize or if the window was not allocated
+	/// surface is also initialized here
+	bool init ()
+	{
+		bool success = true;
+
+		// Initializes SDL sub-systems, if it fails a number below 0 is returned
+		if (SDL_Init(SDL_INIT_VIDEO) < 0)
+		{
+			// SDL_GetError will get an error if one exists
+			cout << "SDL did not initialize properly.\nSDL_Error: " << SDL_GetError() << std::endl;
+			success = false;
+		}
+		else
+		{
+			// Creates the window with the parameters passed into the constructor
+			// We might want to change the SDL_WINDOW_UNDEFINED later, but it defaults to the center of the screen so it should be fine
+			window = SDL_CreateWindow(title.c_str(),
+						  SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, // Here we can define starting window position
+						  width, height,
+						  SDL_WINDOW_SHOWN);
+			if (window != nullptr)
+			{
+				// If we successfully created a window, we can retrieve that window's surface
+				// Surface is the modifiable pixel array of window
+				surface = SDL_GetWindowSurface(window);
+			}
+			else
+			{
+				cout << "SDL could not create a window.\nSDL_Error: " << SDL_GetError() << std::endl;
+				success = false;
+			}
+		}
+		return success;
+	}
+
+	/// Any changes to surface will be rendered when update is called
+	void update ()
+	{
+		SDL_UpdateWindowSurface(window);
+	}
+
+private:
+	SDL_Window * window;
+	SDL_Surface * surface;
+
+	int width, height;
+	string title;
+
+	void close ()
+	{
+		// No need to call SDL_FreeSurface on surface because SDL_DestroyWindow will take care of surface
+		SDL_DestroyWindow(window);
+
+		// Quits SDL systems such as 
+		SDL_Quit();
+	}
+};
